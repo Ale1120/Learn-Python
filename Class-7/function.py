ba368e7ccf25885640dfe4ac321bf6863b3fdf3c
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-09 18:34:37 -0400

    learn fucntion py class-7

---
+++ Class-7/function.py
@@ -0,0 +1,26 @@
+print('function global')
+type(1)
+
+one_int = int('5')
+type(one_int)
+
+a = bool('a')
+print(a)
+
+a = bool('')
+print(a)
+
+a = float('3')
+print(a)
+
+def sum(x, y):
+    return x +y
+
+sum(10,15)
+
+type(sum)
+
+sum_total = sum(10, 15)
+print(sum_total)
+
+type(sum_total)
