aef45d4fa4964e59d83bf38158cef9e3604cac7f
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-10 16:03:55 -0400

    aprendiendo texto y string en python

---
+++ Fase1-Fundamentos-de-programacion/tema1-introduccion-informal/texto.py
@@ -0,0 +1,18 @@
+'Hola Mundo'
+"Hola Mundo"
+
+'hola \"mundo\"'
+
+print('Hola')
+print('mundo')
+
+# tabulacion y salto de linea
+print ('una \ttabulacion')
+print('un\nsalto de linea')
+
+# rout
+print(r"c:\directorio")
+
+#parrafo
+print(""" hola como estas
+bien y tu \t muy bien """)
