81ef75d3c1de5af3151037374ff118af713f5337
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-15 17:57:23 -0400

    aprendiendo operadores logicos or  solo es False cuando todo es False

--- Fase1-Fundamentos-de-programacion/tema2-Operadores-expresiones/logico.py
+++ Fase1-Fundamentos-de-programacion/tema2-Operadores-expresiones/logico.py
@@ -2,6 +2,11 @@
 not True
 not True == False
 
-# verdad operador logico-- and --
+# verdad operador logico -- and --
 True and True
 False and True
+
+# con que uno sea verdad todo es True operador logica -- or --
+True or True
+True or False
+False or False
