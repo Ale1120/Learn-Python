3e3cfb3d8cf0fbd8b6358ae02f2aa5e8ba6841ab
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-15 20:56:07 -0400

    continue con while , y usando else en while

--- Fase1-Fundamentos-de-programacion/tema3-Controladores-de-flujo/while.py
+++ Fase1-Fundamentos-de-programacion/tema3-Controladores-de-flujo/while.py
@@ -15,3 +15,15 @@ while c < 5:
     print('c vale',c)
 else:
     print('se termino c',c)
+
+
+
+c = 0
+while c < 5:
+    c+=1
+    if c==3:
+        print('continuamos c con un continue en ,',c)
+        continue
+    print('c vale',c)
+else:
+    print('se termino c',c)
