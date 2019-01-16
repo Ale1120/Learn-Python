21095ba20e3e34419449d78baf59dfc5fc71c596
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-16 18:34:23 -0400

    modificando una lista con for y enumerate

--- Fase1-Fundamentos-de-programacion/tema3-Controladores-de-flujo/for.py
+++ Fase1-Fundamentos-de-programacion/tema3-Controladores-de-flujo/for.py
@@ -4,5 +4,14 @@ for i in lista:
     print(i)
 
 
-c = 0
+i = 0
 numeros = [1,2,3,4,5,6,7,8,9,10]
+for numero in numeros:
+    numero[i] *= 10
+    i +=1
+numeros
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+for indice,numero in enumerate(lista):
+    numeros[indice] *= 10
+numeros
