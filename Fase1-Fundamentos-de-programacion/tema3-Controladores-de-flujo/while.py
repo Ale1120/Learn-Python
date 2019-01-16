7a31674a64120f5a68f441cb04b7e2fc285b3952
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-15 21:03:53 -0400

    menu con while if, elif break y else

--- Fase1-Fundamentos-de-programacion/tema3-Controladores-de-flujo/while.py
+++ Fase1-Fundamentos-de-programacion/tema3-Controladores-de-flujo/while.py
@@ -27,3 +27,25 @@ while c < 5:
     print('c vale',c)
 else:
     print('se termino c',c)
+
+
+
+print('Bienvenido al menu interactivo')
+
+while(True):
+    print(""" Que quieres hacer?
+    1) Saludar
+    2) Sumar
+    3) Salir""")
+    opcion = input()
+    if opcion == '1':
+        print('hola')
+    elif opcion == '2':
+        n1 = float(input("agrege n1: "))
+        n2 = float(input("agrege n2: "))
+        print("La suma es: ", n1+n2)
+    elif opcion == '3':
+        print('adios...')
+        break
+    else:
+        print('comando erroneo')
