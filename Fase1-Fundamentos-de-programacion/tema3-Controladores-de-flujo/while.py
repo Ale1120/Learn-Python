7468fbf0bbf07ad402d77c44bc2666942c9a1d58
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-15 20:54:33 -0400

    while con break para detenerlo

--- Fase1-Fundamentos-de-programacion/tema3-Controladores-de-flujo/while.py
+++ Fase1-Fundamentos-de-programacion/tema3-Controladores-de-flujo/while.py
@@ -4,3 +4,14 @@ while c < 5:
     print('c vale',c)
 else:
     print('se termino c',c)
+
+
+c = 0
+while c < 5:
+    c+=1
+    if c==2:
+        print('detenimos c con un break en ,',c)
+        break
+    print('c vale',c)
+else:
+    print('se termino c',c)
