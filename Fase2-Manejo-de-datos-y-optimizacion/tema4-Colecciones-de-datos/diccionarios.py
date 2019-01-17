f96521d33a8d2596d911f2ad84f26dc489871bf3
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-17 15:24:06 -0400

    borrar un elemento del diccionario

--- Fase2-Manejo-de-datos-y-optimizacion/tema4-Colecciones-de-datos/diccionarios.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema4-Colecciones-de-datos/diccionarios.py
@@ -2,5 +2,9 @@
 colores = {'amarillo':'yellow', 'azul':'blue', 'verde':'green' }
 
 color['amarillo']
+
 # modificando un elemento
 colores['amarillo']= 'white'
+
+# borrar un elemento clave:valor
+del(colores['amarillo'])
