85b8454ded4e762486bc5a3cc01fa60a7b70c41e
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-16 20:27:15 -0400

    count con tuplas para saber cuantas veces esta un elemento

--- Fase2-Manejo-de-datos-y-optimizacion/tema4-Colecciones-de-datos/tuplas.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema4-Colecciones-de-datos/tuplas.py
@@ -1,5 +1,5 @@
 # son se pueden modificar
-tupla = (100,200,'hola',[1,2,3],-4)
+tupla = (100,200,'hola',[1,2,3],-4,100)
 tupla
 tupla[0]
 tupla[2:]
@@ -9,3 +9,6 @@ len(tupla)
 
 # index para buscar un elemento y su posicion
 tuplas.index(200)
+
+# count para saber cuantas veces esta un elemento
+tupla.count()
