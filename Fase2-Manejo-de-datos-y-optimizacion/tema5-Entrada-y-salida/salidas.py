943ef0151c996520035dec8bad0247c19a6e4c86
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-17 22:37:54 -0400

    agregando condicionales

--- Fase2-Manejo-de-datos-y-optimizacion/tema5-Entrada-y-salida/salidas.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema5-Entrada-y-salida/salidas.py
@@ -1,5 +1,9 @@
 import sys
-texto = sys.argv[1]
-repeticiones = int(sys.argv[2])
-for i in range(repeticiones):
-    print(texto)
+if len(sys.argv) == 3:
+    texto = sys.argv[1]
+    repeticiones = int(sys.argv[2])
+    for i in range(repeticiones):
+        print(texto)
+else:
+    print('error sobrepaso el maximo de argumento permitidos')
+    print("ejemplo script.py 'hola' 5")
