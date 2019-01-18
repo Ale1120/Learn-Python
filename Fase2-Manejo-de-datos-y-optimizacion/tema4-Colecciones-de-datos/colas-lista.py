1ad40d40474b986d037e515b769218d74c2b14fc
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-17 21:44:04 -0400

    creando una cola con lista en py

--- Fase2-Manejo-de-datos-y-optimizacion/tema4-Colecciones-de-datos/colas-lista.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema4-Colecciones-de-datos/colas-lista.py
@@ -1,1 +1,4 @@
-# FIFO primero entrar, primero en salir
+# FIFO primero entrar, primero en salir deque para usar colas en py
+
+from  collections import deque
+cola = deque(['alejandro','jose','maria'])
