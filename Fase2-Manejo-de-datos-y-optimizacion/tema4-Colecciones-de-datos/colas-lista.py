2362fb2a33a6fa1d363e88fbb41921f89d86f85f
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-17 21:46:10 -0400

    utilizando popleft para sacar elemento de una cola con popleft()

--- Fase2-Manejo-de-datos-y-optimizacion/tema4-Colecciones-de-datos/colas-lista.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema4-Colecciones-de-datos/colas-lista.py
@@ -2,3 +2,8 @@
 
 from  collections import deque
 cola = deque(['alejandro','jose','maria'])
+cola.app('juan')
+cola.app('javier')
+
+# sacar el valor con popleft para usar colas
+p = cola.popleft()
