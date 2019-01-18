9780fb765bd7d7a588738cac579b9d17f1d185f5
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-17 22:11:26 -0400

    ejerccio 3 cola usando lista en python

--- Fase2-Manejo-de-datos-y-optimizacion/tema4-Colecciones-de-datos/ejerccios/ejerccio.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema4-Colecciones-de-datos/ejerccios/ejerccio.py
@@ -1,4 +1,4 @@
-# ejerccio 1
+# ejerccio 1 coleccion
 usuario = {'marta','david','elvira', 'juan', 'marcos'}
 admin = {'juan', 'marta'}
 # para sacar un elemento de una coleccion
@@ -11,7 +11,7 @@ for i in usuario:
     else:
         print(i,'no es admin')
 
-# ejerccio 2
+# ejerccio 2 diccionario
 
 caballero = {'vida':2, 'ataque':2, 'defensa':2, 'alcance':2}
 guerrero = {'vida':2, 'ataque':2, 'defensa':2, 'alcance':2}
@@ -31,3 +31,30 @@ arquero['alcance'] = guerrero['alcance']*2
 print('Caballero:\t', caballero)
 print('guerrero:\t', guerrero)
 print('arquero:\t', arquero)
+
+# ejerccio 3 cola
+from collections import deque
+
+tareas = [
+    [6,'Distribucion'],
+    [2,'Diseño'],
+    [1,'Concepcion'],
+    [7,'Mantenimiento'],
+    [4,'Produccion'],
+    [3,'Planidicacion'],
+    [5,'Prueba']
+]
+print("==Tareas desordenadas==")
+for i in tareas:
+    print(i[0],i[1])
+
+# ordenar una lista
+tareas.sort()
+cola =duque()
+
+for i in tareas:
+    cola.append(i[1])
+
+print("==Tareas Ordenadas==")
+for i in tareas:
+    print(i)
