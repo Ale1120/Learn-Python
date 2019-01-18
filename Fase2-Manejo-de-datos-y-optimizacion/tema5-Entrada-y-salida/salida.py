8c0a3c7a8b8b2d7b993c66d972bb55b44dbfb725
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-18 10:59:26 -0400

    usando format con referencias para ser el codigo mas legible

--- Fase2-Manejo-de-datos-y-optimizacion/tema5-Entrada-y-salida/salida.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema5-Entrada-y-salida/salida.py
@@ -4,3 +4,8 @@ print('un Texto',v,'y un numero', n)
 
 # manera de hacerlo mas eficiente de mostrar informacion
 c = 'un Texto {} y un numero  {}'.format(v,n)
+
+print('un Texto {0} y un numero  {1}'.format(v,n))
+
+print('un Texto {texto} y un numero  {numero}'.format(texto=v,numero=n))
+print('un Texto {v} y un numero  {n}'.format(v,n))
