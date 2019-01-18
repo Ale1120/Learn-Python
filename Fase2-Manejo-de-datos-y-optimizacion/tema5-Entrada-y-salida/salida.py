dfb2601b1a0f3daf6555ad84910dbf598c51cb9d
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-17 22:26:55 -0400

    importando sys y usando argv

--- Fase2-Manejo-de-datos-y-optimizacion/tema5-Entrada-y-salida/salida.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema5-Entrada-y-salida/salida.py
@@ -1,3 +1,7 @@
 # scripts
 # instrucciones en codigo fuente que se ejecutan de arriba a abajo guardando
 # en un fichero con nombre unico y ejecutados desde el interprete
+
+import sys
+print('hola mundo')
+print(sys.argv)
