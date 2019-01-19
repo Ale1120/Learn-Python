eb75e16282d4546949f5893c6312ad75e2a44646
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-19 14:34:27 -0400

    ** kwargs generando un diccionario

--- Fase2-Manejo-de-datos-y-optimizacion/tema6-Funciones/argumentos-parametros-indeterminados.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema6-Funciones/argumentos-parametros-indeterminados.py
@@ -2,5 +2,12 @@
 def indeterminados_posicion(*args):
     for i in args:
         print(arg)
-        
+
 indeterminados_posicion(5,'hola',[1,2])
+
+# **kwargs genera un diccionario
+def indeterminados_posicion( **kwargs):
+    for i in kwargs:
+        print(kwargs,'',kwargs[i])
+
+indeterminados_posicion(n=5,c='hola',l=[1,2])
