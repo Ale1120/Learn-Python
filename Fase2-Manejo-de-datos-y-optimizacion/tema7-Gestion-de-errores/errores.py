555c24103a66eb990395bc0389e606dffca59897
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-19 18:32:46 -0400

    errores comunes en python

---
+++ Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/errores.py
@@ -0,0 +1,26 @@
+print('hola'
+
+pint('hola')
+
+# errores semanticos
+lista = [1,2,3]
+l.pop()
+l.pop()
+l.pop()
+l.pop()
+
+# comprovar
+lista = [1,2,3]
+if len(lista) > 0:
+    lista.pop()
+
+# error simanticos
+numero = input('agregue un numero:')
+numero2 = 4
+print("{}/{}={}".format(n,m,n/m))
+
+
+# solucion
+numero = float(input('agregue un numero: '))
+numero2 = 4
+print("{}/{}={}".format(n,m,n/m))
