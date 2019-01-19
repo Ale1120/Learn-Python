6d685ac8a69b5aa89349e070b0b2337b99eddb86
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-19 16:15:47 -0400

    funciones integradas

---
+++ Fase2-Manejo-de-datos-y-optimizacion/tema6-Funciones/integradas.py
@@ -0,0 +1,32 @@
+n= int('10')
+n
+f= float('10.5')
+f
+s= 'un texto y un numero' + str(10) + ',' + str(3.14)
+s
+bin(10)
+hex(10)
+# de binario a entero
+int('0b1010',2)
+
+# de hexadecimal a entero
+int('0xa',16)
+
+# valor de  absoluto
+abs(-10)
+
+# redondeo d enumero
+round(5.5)
+round(5.4)
+
+# evaluar string
+eval('2+5')
+n = 10
+eval('n*10-5')
+
+# longitud de string
+
+len('una cadena')
+
+#ayuda
+help()
