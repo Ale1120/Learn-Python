d0d4083497c2a0a1b6afac9bb2f457e14fab33cd
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-19 18:03:39 -0400

    mayor, igual y menor ejerccio 3

--- Fase2-Manejo-de-datos-y-optimizacion/tema6-Funciones/ejerccios/ejerccios.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema6-Funciones/ejerccios/ejerccios.py
@@ -10,3 +10,16 @@ def area_circulo(r):
     retrun (r**2)* math.pi
 
 print(area_circulo(5))
+
+# ejerccio 3
+
+def relaccion(a,b):
+    if a > b:
+        return 1
+    elif a < b :
+        return -1
+    else:
+        return 0
+relaccion(5,10)
+relaccion(10,5)
+relaccion(5,5)
