07914b1288cca61b9af9faf931aba0082deb8b3b
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-19 14:03:01 -0400

    asignando valor por defecto a parametros en funcion

--- Fase2-Manejo-de-datos-y-optimizacion/tema6-Funciones/argumentos-parametros.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema6-Funciones/argumentos-parametros.py
@@ -6,3 +6,12 @@ resta(1,2)
 
 # por asignacion
 resta(b=1,a=2)
+
+# valor por defecto  a parametros
+def resta(a=None,b=None):
+    if a == None or b == None:
+        print('argumentos vacio por favor agrege dos argumentos')
+    else:
+        return a-b
+
+resta()
