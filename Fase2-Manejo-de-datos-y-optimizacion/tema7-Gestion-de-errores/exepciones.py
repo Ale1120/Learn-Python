3d61d2e31ba93129dec66cf7caa659e42db46307
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-22 12:03:13 -0400

    agregando else al while

--- Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/exepciones.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/exepciones.py
@@ -11,6 +11,9 @@ while(True):
         numero = float(input('agregue un numero: '))
         numero2 = 4
         print("{}/{}={}".format(numero,numero2,numero/numero2))
-        break # romper la el while si sale bn
+
     except:
         print('Ha ocurrido un error, agregue un numero')
+    else:
+        print('Todo ha funcionando correctamente')
+        break # romper la el while si sale bn
