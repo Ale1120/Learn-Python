56c27faa291a91f79c183b6cd21d69b4bb7ff289
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-22 12:10:01 -0400

    agregando except TypeError para ejecutar algo con este tipo derror

--- Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/execpciones-multiples.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/execpciones-multiples.py
@@ -1,5 +1,7 @@
 try:
     n = input('Introdusca un numero: ')
     5/n
+except TypeError:
+    print('no se puede dividir un numeor con un string')
 except Exception as e:
     print(type(e).__name__ )
