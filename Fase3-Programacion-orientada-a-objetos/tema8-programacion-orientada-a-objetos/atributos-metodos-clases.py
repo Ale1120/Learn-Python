0b9f890eb557ab128a378edcd307f192ef1261e2
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-22 19:31:29 -0400

    ejemplo de uso de self

--- Fase3-Programacion-orientada-a-objetos/tema8-programacion-orientada-a-objetos/atributos-metodos-clases.py
+++ Fase3-Programacion-orientada-a-objetos/tema8-programacion-orientada-a-objetos/atributos-metodos-clases.py
@@ -25,3 +25,60 @@ galleta = Galleta()
 galleta.chocolate
 
 # ejemplo
+
+class Galleta:
+    chocolate = False
+
+    def __init__ (self):
+        print('Se acaba de crear una galleta')
+
+    def chocolatear(self):
+        self.chocolate = True
+
+    def tiene_chocolate(self):
+        if self.chocolate:
+            print('soy una galleta con chocolate')
+        else:
+            print('soy una galleta sin chocolate')
+galleta = Galleta()
+galleta.tiene_chocolate()
+galleta.chocolatear()
+galleta.tiene_chocolate()
+galleta.chocolate
+
+class Galleta:
+    chocolate = False
+
+    def __init__ (self,sabor,forma):
+        self.sabor = sabor
+        self.forma = forma
+        print('Se acaba de crear una galleta {} {}'.format(sabor,forma))
+
+    def chocolatear(self):
+        self.chocolate = True
+
+    def tiene_chocolate(self):
+        if self.chocolate:
+            print('soy una galleta con chocolate')
+        else:
+            print('soy una galleta sin chocolate')
+
+galleta('salada','redonda')
+
+class Galleta:
+    chocolate = False
+
+    def __init__ (self,sabor=None, forma=None):
+        self.sabor = sabor
+        self.forma = forma
+        if sabor is not None and forma is not None:
+            print('Se acaba de crear una galleta {} {}'.format(sabor,forma))
+
+    def chocolatear(self):
+        self.chocolate = True
+
+    def tiene_chocolate(self):
+        if self.chocolate:
+            print('soy una galleta con chocolate')
+        else:
+            print('soy una galleta sin chocolate')
