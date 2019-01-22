131d3ce1b9f4f98a2d68c633b577d78e3b54bb7e
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-22 18:57:12 -0400

    creando una clase

--- Fase3-Programacion-orientada-a-objetos/tema8-programacion-orientada-a-objetos/estructurada-vs-POO.py
+++ Fase3-Programacion-orientada-a-objetos/tema8-programacion-orientada-a-objetos/estructurada-vs-POO.py
@@ -1,13 +1,13 @@
 # estructurada
 clientes = [
-    {'Nombre':'Alejandro', 'Apellido':'Ortegano','CI':'1516628'},
-    {'Nombre':'Anthony', 'Apellido':'Sanoja','CI':'2216628'}
+    {'Nombre':'Alejandro', 'Apellido':'Ortegano','ci':'1516628'},
+    {'Nombre':'Anthony', 'Apellido':'Sanoja','ci':'2216628'}
 ]
 
 def mostrar_cliente(clientes,ci):
     for cliente in clientes:
-        if (ci == cliente['CI']):
-            print('{}{}'.formact(cliente['Nombre'],cliente['Apellido']))
+        if (ci == cliente['ci']):
+            print('{} {}'.format(cliente['Nombre'],cliente['Apellido']))
             return
     print('Cliente no encontrado')
 
@@ -17,7 +17,38 @@ def borrar_cliente(clientes,ci):
     for i,c in enumerate(clientes):
         if(ci == c['ci']):
             del(clientes[i])
-            print(str()), '> BORRADO'
+            print(str(c), '> BORRADO')
             return
     print('Cliente no encontrado')
- 
+
+borrar_cliente(clientes,'2216628')
+
+# POO
+
+class Cliente:
+    def __init__(self,ci,nombre,apellido):
+        self.ci = ci
+        self.nombre = nombre
+        self.apellido = apellido
+
+    def __str__(self):
+        return '{} {}'.format(self.nombre,self.apellido)
+
+class Empresa:
+    def __init__(self,clientes=[]):
+        self.clientes = clientes
+
+    def mostrar_cliente(self,ci=None):
+        for cliente in self.clientes:
+            if cliente.ci == ci:
+                print(cliente)
+                return
+        print('Cliente no encontrado')
+
+    def borrar_cliente(self,ci=None):
+        for i,c in enumerate(self.clientes):
+            if c.ci == ci:
+                del(self.clientes[i])
+                print(str(c), '> BORRADO')
+                return
+        print('Cliente no encontrado')
