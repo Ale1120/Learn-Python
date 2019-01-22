af2e6f753e8c86d89c645640fbc1204a4d3b4efe
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-22 12:05:18 -0400

    agregando finally usando con try,except

--- Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/exepciones.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/exepciones.py
@@ -7,13 +7,14 @@ except:
 
 
 while(True):
-    try:
+    try: # para capturar un error
         numero = float(input('agregue un numero: '))
         numero2 = 4
         print("{}/{}={}".format(numero,numero2,numero/numero2))
-
-    except:
+    except: # codigo exepcional
         print('Ha ocurrido un error, agregue un numero')
     else:
         print('Todo ha funcionando correctamente')
         break # romper la el while si sale bn
+    finally: # para ejecutar el codigo que se ejecutara al final
+        print('Fin de la interacion')
