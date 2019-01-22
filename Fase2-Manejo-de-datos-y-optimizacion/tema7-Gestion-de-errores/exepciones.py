da469023fd0662061e80eb3baca08d8fbe1c1d73
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-22 12:01:53 -0400

    creando una expcion con try y except

--- Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/exepciones.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/exepciones.py
@@ -1,6 +1,16 @@
 try:
     numero = float(input('agregue un numero: '))
     numero2 = 4
-    print("{}/{}={}".format(n,m,n/m))
+    print("{}/{}={}".format(numero,numero2,numero/numero2))
 except:
     print('Ha ocurrido un error, agregue un numero')
+
+
+while(True):
+    try:
+        numero = float(input('agregue un numero: '))
+        numero2 = 4
+        print("{}/{}={}".format(numero,numero2,numero/numero2))
+        break # romper la el while si sale bn
+    except:
+        print('Ha ocurrido un error, agregue un numero')
