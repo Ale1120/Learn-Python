f6129b1129bb5654e40eb09f02732379e2d40b19
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-22 12:12:47 -0400

    agregando varios tipos derrores como  ValueError, ZeroDivisionError

--- Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/execpciones-multiples.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/execpciones-multiples.py
@@ -1,7 +1,11 @@
 try:
-    n = input('Introdusca un numero: ')
+    n = float(input('Introdusca un numero: '))
     5/n
 except TypeError:
     print('no se puede dividir un numeor con un string')
+except ValueError:
+    print('Solo se perminte numeros')
+except ZeroDivisionError:
+    print('no se puede dividir por cero')
 except Exception as e:
     print(type(e).__name__ )
