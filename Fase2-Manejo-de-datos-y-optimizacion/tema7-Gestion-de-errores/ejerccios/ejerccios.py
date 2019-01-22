6058f923393f06a6c0fb7c3d80424a3220b0748f
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-22 12:24:49 -0400

    ejerccio 2 except Indexerror

--- Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/ejerccios/ejerccios.py
+++ Fase2-Manejo-de-datos-y-optimizacion/tema7-Gestion-de-errores/ejerccios/ejerccios.py
@@ -3,3 +3,10 @@ try:
     resultado = 10/0
 except ZeroDivisionError:
     print('no se puede dividir por cero')
+
+# ejerccio 2
+try:
+    lista = [1,2,3,4,5]
+    lista[10]
+except IndexError:
+    print('Error el index supera la longuitud de la lista',len(lista))
