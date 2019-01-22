4b08cfd72e115d613256045d6e382c8754c7bf49
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-22 19:16:41 -0400

    creando __init__ y self

---
+++ Fase3-Programacion-orientada-a-objetos/tema8-programacion-orientada-a-objetos/atributos-metodos-clases.py
@@ -0,0 +1,15 @@
+class Galleta:
+    pass
+
+galleta = Galleta()
+
+galleta.sabor = 'Salado'
+galleta.color = 'Marron'
+
+print('El sabor de una galleta es ', galleta.sabor)
+
+class Galleta:
+    chocolate = False
+
+galleta = Galleta()
+g.chocolate
