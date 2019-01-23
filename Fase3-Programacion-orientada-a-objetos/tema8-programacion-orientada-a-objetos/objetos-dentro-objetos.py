a6098f43561eef11e1e6be6413091bb88ebcc759
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-23 15:47:53 -0400

    creando y utilizando la clase catalogo

--- Fase3-Programacion-orientada-a-objetos/tema8-programacion-orientada-a-objetos/objetos-dentro-objetos.py
+++ Fase3-Programacion-orientada-a-objetos/tema8-programacion-orientada-a-objetos/objetos-dentro-objetos.py
@@ -1,4 +1,4 @@
-class Pelicula(object):
+class Pelicula:
     # Constructor de la classe
     def __init__(self, titulo, duracion, lanzamiento):
         self.titulo = titulo
@@ -8,3 +8,21 @@ class Pelicula(object):
 
     def __str__(self):
         return "{} ({})".format(self.titulo,self.lanzamiento)
+
+class Catalogo:
+     peliculas = []
+
+    def __init__(self,peliculas[]):
+        self.peiculas = peliculas
+
+    def agregar(self,pelicula):
+        self.peliculas.append(peliculas)
+
+    def mostrar(self):
+        for pelicula in self.peliculas:
+            print(pelicula)
+
+
+padrino = Pelicula("El padrino", 175,1972)
+accion = Catalogo([padrino])
+accion.mostrar()
