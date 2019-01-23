2b222f400c87f8e531a709d99a7241f3cf822186
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-23 16:17:03 -0400

    utilizando metodos publico para usar metodos privado antraves de ellos

--- Fase3-Programacion-orientada-a-objetos/tema8-programacion-orientada-a-objetos/encapsulacion-atributos-metodos.py
+++ Fase3-Programacion-orientada-a-objetos/tema8-programacion-orientada-a-objetos/encapsulacion-atributos-metodos.py
@@ -7,3 +7,22 @@ class Ejemplo:
 e = Ejemplo()
 e.__atributo_privado
 e.__metodo_privado()
+
+# ejemplo con puente para usar metod privados dentro de otro __metodo_privado
+
+class Ejemplo:
+    __atributo_privado = "soy un atributo inalcanzable desde afuera"
+
+    def __metodo_privado(self):
+        print("soy un metodo inalcanzable desde afuera")
+
+    def atributo_publico(self):
+        return self.__atributo_privado
+
+    def metodo_publico(self):
+        return self.__metodo_privado()
+
+
+e = Ejemplo()
+e.atributo_publico
+e.metodo_publico()
