5b1aae9d68b1be8d29cfc9c602f3a7351e92d111
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-23 19:48:31 -0400

    creando super clase producto

--- Fase3-Programacion-orientada-a-objetos/tema9-Herencia-POO/herencia.py
+++ Fase3-Programacion-orientada-a-objetos/tema9-Herencia-POO/herencia.py
@@ -3,6 +3,7 @@ class Producto:
     productor=None, distribuidor=None,codigo=None,autor=None):
         self.referencia = referencia
         self.tipo = tipo
+        self.nombre = nombre
         self.precio = precio
         self.descripcion = descripcion
         self.productor = productor
@@ -10,3 +11,20 @@ class Producto:
         self.autor = autor
 
 adorno = Producto('00A','ADORNO', 'Vaso Adornado',15,'Vaso de porcelana con dibujos')
+
+
+# Herencias
+# Super clase
+class Producto:
+    def __init__(self,referencia,nombre,precio,descripcion):
+        self.referencia = referencia
+        self.nombre = nombre
+        self.precio = precio
+        self.descripcion = descripcion
+
+    def __str__(self):
+        return """
+        REFERENCIA\t{}
+        NOMBRE\t{}
+        PRECIO\t{}
+        DESCRIPCION\t{}""".format(self.referencia,self.nombre,self.precio,self.descripcion)
