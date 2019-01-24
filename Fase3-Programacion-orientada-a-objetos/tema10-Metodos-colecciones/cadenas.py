a7e6287628639c7d4a2e0cd4d32fc685b12a7963
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-24 19:28:18 -0400

    isdigit() digitos, isalnum() alfanumericos, isalpha() letras

--- Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/cadenas.py
+++ Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/cadenas.py
@@ -12,3 +12,21 @@
 
 # contar las veces que aparece un caracter
 "Hola mundo".count('o')
+
+# para ver en que posicion comienza un aplabra
+"Hola mundo".find('mundo')
+
+# contar al de atras hacia adelante
+"Hola mundo mundo mundo".rfind('mundo')
+
+c = '100'
+# para ver si es un digito
+c.isdigit()
+
+# para saber si todo es alfanumerico
+c2 = "ABC123f"
+c2.isalnum()
+
+# solo si tenemos letra (los espacio no cuenta)
+c3 = "AAAAVVaaa"
+c3.isalpha()
