a6f4f31d29d9bc95a935631a3fd86681cbb4fba9
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-24 15:54:42 -0400

    llamando metodos de la clase padre en la clase hija

--- Fase3-Programacion-orientada-a-objetos/tema9-Herencia-POO/ejerccios/ejerccio.py
+++ Fase3-Programacion-orientada-a-objetos/tema9-Herencia-POO/ejerccios/ejerccio.py
@@ -24,3 +24,28 @@ class Coche(Vehiculo):
 
 coche = Coche("azul",150,4,1200)
 print(coche)
+
+# llamando el metodo de la clase
+class Vehiculo():
+
+    def __init__(self,color,ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return "color {}, {} ruedas".format(self.color, self.ruedas)
+
+
+class Coche(Vehiculo):
+
+    def __init__(self,color,ruedas,velocidad,cilindrada):
+        Vehiculos.__init__(self,color,ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return Vehiculo.__str__(self) + ",km/h, {} ruedas, {} cc".
+        format(self.velocidad,self.cilindrada)
+
+coche = Coche("azul",150,4,1200)
+print(coche)
