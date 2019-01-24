b3c313f365cf5b56113c1c6744ee22999f831216
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-23 20:04:48 -0400

    creando clase hija alimentos

--- Fase3-Programacion-orientada-a-objetos/tema9-Herencia-POO/herencia.py
+++ Fase3-Programacion-orientada-a-objetos/tema9-Herencia-POO/herencia.py
@@ -1,18 +1,3 @@
-class Producto:
-    def __init__(self,referencia,tipo,nombre,precio,descripcion,
-    productor=None, distribuidor=None,codigo=None,autor=None):
-        self.referencia = referencia
-        self.tipo = tipo
-        self.nombre = nombre
-        self.precio = precio
-        self.descripcion = descripcion
-        self.productor = productor
-        self.codigo = codigo
-        self.autor = autor
-
-adorno = Producto('00A','ADORNO', 'Vaso Adornado',15,'Vaso de porcelana con dibujos')
-
-
 # Herencias
 # Super clase
 class Producto:
@@ -23,12 +8,33 @@ class Producto:
         self.descripcion = descripcion
 
     def __str__(self):
-        return """
-        REFERENCIA\t{}
-        NOMBRE\t{}
-        PRECIO\t{}
-        DESCRIPCION\t{}""".format(self.referencia,self.nombre,self.precio,self.descripcion)
+        return """\
+REFERENCIA\t{}
+NOMBRE\t\t{}
+PRECIO\t\t{}
+DESCRIPCION\t\t{}""".format(self.referencia,self.nombre,self.precio,self.descripcion)
 
 # clases hijas
-class adorno(Producto):
+class Adorno(Producto):
     pass
+
+adorno = Adorno(1245, 'Vaso Adornado',15,'Vaso de porcelana con dibujos')
+print(adorno)
+
+class Alimento(Producto):
+    productor = ""
+    distribuidor = ""
+    def __str__(self):
+        return """\
+REFERENCIA\t{}
+NOMBRE\t\t{}
+PRECIO\t\t{}
+DESCRIPCION\t{}
+PRODUCTOR\t{}
+DISTRIBUIDOR\t{}""".format(self.referencia,self.nombre,self.precio,self.descripcion,
+self.productor, self.distribuidor)
+
+alimento = Alimento(2035,"Botella de Aceite de Olivar",5,"250 Mil")
+alimento.productor="La aceitera"
+alimento.distribuidor="polar"
+print(alimento)
