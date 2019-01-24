fea1c7739ccdd380f56ff433c6d51602934e82ca
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-24 15:59:13 -0400

    usando super(), y viendo la introduccion del ejerccio tema 9

--- Fase3-Programacion-orientada-a-objetos/tema9-Herencia-POO/ejerccios/ejerccio.py
+++ Fase3-Programacion-orientada-a-objetos/tema9-Herencia-POO/ejerccios/ejerccio.py
@@ -39,12 +39,38 @@ class Vehiculo():
 class Coche(Vehiculo):
 
     def __init__(self,color,ruedas,velocidad,cilindrada):
-        Vehiculos.__init__(self,color,ruedas)
+        Vehiculo.__init__(self,color,ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
 
     def __str__(self):
         return Vehiculo.__str__(self) + ",km/h, {} ruedas, {} cc".
+        format(self.velocidad,self.cilindrada)
+
+coche = Coche("azul",150,4,1200)
+print(coche)
+
+# usando super() para ahorrar codigo
+# llamando el metodo de la clase para no usar el self
+class Vehiculo():
+
+    def __init__(self,color,ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return "color {}, {} ruedas".format(self.color, self.ruedas)
+
+
+class Coche(Vehiculo):
+
+    def __init__(self,color,ruedas,velocidad,cilindrada):
+        super().__init__(color,ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return super().__str__() + ",km/h, {} ruedas, {} cc".
         format(self.velocidad,self.cilindrada)
 
 coche = Coche("azul",150,4,1200)
