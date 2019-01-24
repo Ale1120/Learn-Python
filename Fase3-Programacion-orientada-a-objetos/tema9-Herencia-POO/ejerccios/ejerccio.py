b711369da9087dacd314a67604cc9154f92c5991
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-24 16:36:58 -0400

    creando clases camion bicicleta y motocicletas ejerccios tema 9

--- Fase3-Programacion-orientada-a-objetos/tema9-Herencia-POO/ejerccios/ejerccio.py
+++ Fase3-Programacion-orientada-a-objetos/tema9-Herencia-POO/ejerccios/ejerccio.py
@@ -13,58 +13,6 @@ class Vehiculo():
 class Coche(Vehiculo):
 
     def __init__(self,color,ruedas,velocidad,cilindrada):
-        self.color = color
-        self.ruedas = ruedas
-        self.velocidad = velocidad
-        self.cilindrada = cilindrada
-
-    def __str__(self):
-        return "color {}, {} km/h, {} ruedas, {} cc".format(self.color,
-        self.velocidad ,self.ruedas, self.cilindrada)
-
-coche = Coche("azul",150,4,1200)
-print(coche)
-
-# llamando el metodo de la clase
-class Vehiculo():
-
-    def __init__(self,color,ruedas):
-        self.color = color
-        self.ruedas = ruedas
-
-    def __str__(self):
-        return "color {}, {} ruedas".format(self.color, self.ruedas)
-
-
-class Coche(Vehiculo):
-
-    def __init__(self,color,ruedas,velocidad,cilindrada):
-        Vehiculo.__init__(self,color,ruedas)
-        self.velocidad = velocidad
-        self.cilindrada = cilindrada
-
-    def __str__(self):
-        return Vehiculo.__str__(self) + ",km/h, {} ruedas, {} cc".
-        format(self.velocidad,self.cilindrada)
-
-coche = Coche("azul",150,4,1200)
-print(coche)
-
-# usando super() para ahorrar codigo
-# llamando el metodo de la clase para no usar el self
-class Vehiculo():
-
-    def __init__(self,color,ruedas):
-        self.color = color
-        self.ruedas = ruedas
-
-    def __str__(self):
-        return "color {}, {} ruedas".format(self.color, self.ruedas)
-
-
-class Coche(Vehiculo):
-
-    def __init__(self,color,ruedas,velocidad,cilindrada):
         super().__init__(color,ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
@@ -73,5 +21,33 @@ class Coche(Vehiculo):
         return super().__str__() + ",km/h, {} ruedas, {} cc".
         format(self.velocidad,self.cilindrada)
 
-coche = Coche("azul",150,4,1200)
-print(coche)
+class Camioneta(Coche):
+
+    def __init__(self,color,ruedas,velocidad,cilindrada,carga):
+        super().__init__(color,ruedas,velocidad,cilindrada)
+        self.carga = carga
+
+    def __str__(self):
+        return super().__str__() + "{} kg de cargas".
+        format(self.carga)
+
+class Bicicleta(Vehiculo):
+
+    def __init__(self,color,ruedas,tipo):
+        super().__init__(color,ruedas)
+        self.tipo = tipo
+
+    def __str__(self):
+        return super().__str__() + "{} ".
+        format(self.tipo)
+
+class Motocicleta(Bicicleta):
+
+    def __init__(self,color,ruedas,tipo,velocidad,cilindrada):
+        super().__init__(color,ruedas,tipo)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return super().__str__() + ",km/h, {} ruedas, {} cc".
+        format(self.velocidad,self.cilindrada)
