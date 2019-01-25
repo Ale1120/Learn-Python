e7e63afa9557907d3592a5704a289666cce9f795
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-25 17:19:23 -0400

    pop() y clear() borrando por keys y borrando diccionario completo

--- Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/diccionarios.py
+++ Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/diccionarios.py
@@ -3,16 +3,28 @@ colores = {"amarillo":"yellow", "azul":"blue", "verde":"green"}
 colores['amarillo']
 
 # mostrar elemento
-print("mostrar elemento con get()",colores.get('amarillo',"no se encuentra"))
+print("Mostrar elemento con get()",colores.get('amarillo',"no se encuentra"))
 
 # ver si una keys se  encuentra en el diccionario
 "amarillo" in colores
 
 # mostrar keys
-print("mostanado las clave de un diccionario keys():",colores.keys())
+print("Mostanado las clave de un diccionario keys():",colores.keys())
 
 # mostrar valores
-print("mostanado las valores de un diccionario values():",colores.values())
+print("Mostanado las valores de un diccionario values():",colores.values())
 
 # mostrar keys y values
 print("Mostando keys y values en tuplas con item(): ",colores.item())
+
+# recorrer un diccionario
+for clave, valor in colores.item():
+    print("El keys {} y el values {} son del diccionario colores!!".format(clave,valor))
+
+# borrar un elemento con su keys
+colores.pop("amarillo","no se ha encontrado")
+print("El keys que fue eliminado es el amarillo",colores)
+
+# borrar todo el diccionarios
+colores.clear()
+print("El Diccionario colores esta vacio",colores)
