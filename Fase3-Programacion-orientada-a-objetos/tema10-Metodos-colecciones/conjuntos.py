e83c415b5b9460961f3dbffd5da94d85d01df9c6
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-25 15:52:02 -0400

    difference()diferenciar elementosconjutos difference_update() actualizar

--- Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/conjuntos.py
+++ Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/conjuntos.py
@@ -35,4 +35,15 @@ c4.issuperset(c1)
 # union de dos conjuntos
 print("union de c1 con c2",c1.union(c2))
 # actualizando el conjuto con union
-c1.update(c2)
+print("actualizando y union en c1 y c2 :",c1.update(c2))
+
+# diferencia elemento de conjutos
+c1 = {1,2,3}
+c2 = {3,4,5}
+c3 = {-1,99}
+c4 = {1,2,3,4,5}
+print("los elementos distinto del c1 y c2 son : ",c1.difference(c2))
+
+# diferencia  y actualizar
+c1.difference_update(c2)
+print("los elementos distintos del c1 y c2 son :",c1)
