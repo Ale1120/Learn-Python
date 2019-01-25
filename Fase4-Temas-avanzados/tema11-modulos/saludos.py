608b0631050c36f7173a843742f6913fce820d33
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-25 18:26:07 -0400

    agregando una clase saludo

--- Fase4-Temas-avanzados/tema11-modulos/saludos.py
+++ Fase4-Temas-avanzados/tema11-modulos/saludos.py
@@ -2,6 +2,7 @@
 def saludar():
     print("hola, te estoy saludando desde la funcion saludar del modulo saludos")
 
+
 class Saludo():
-    def __init_(self):
+    def __init__(self):
         print("Hola te estoy saludando desde el __init__ de la clase del modulo saludos")
