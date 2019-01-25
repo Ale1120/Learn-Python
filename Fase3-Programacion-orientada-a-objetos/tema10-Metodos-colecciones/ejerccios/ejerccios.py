b9ea435bf93382974341ff683c71640aeefbdc34
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-25 17:42:56 -0400

    resolviendo el ejerccio 1 con split() y capitalize()

--- Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/ejerccios/ejerccios.py
+++ Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/ejerccios/ejerccios.py
@@ -1,2 +1,14 @@
 # ejerccios 1
-texto = "un dia que el viedo soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viendo -respondio otro monje#ni las banderolas ni el viento lo que s emueve son vuestras mente -dijo el maesto."
+texto = "un dia que el viedo soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viendo -respondio otro monje#ni las banderolas ni el viento lo que se mueve son vuestras mente -dijo el maestro"
+
+lineas = texto.split("#")
+
+for i,linea in enumerate(lineas):
+    lineas[i] = linea.capitalize()
+    if i == 0:
+        lineas[i] = lineas[i] + "..."
+    else:
+        lineas[i] = "- " +lineas[i] + "."
+
+for linea in lineas:
+    print(linea)
