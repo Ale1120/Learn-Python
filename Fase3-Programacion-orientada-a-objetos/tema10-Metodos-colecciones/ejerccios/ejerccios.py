270d534eaf64dfda97f7f952dd49b7acb9463a22
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-25 17:36:26 -0400

    introduccion al ejerccio 1

---
+++ Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/ejerccios/ejerccios.py
@@ -0,0 +1,2 @@
+# ejerccios 1
+texto = "un dia que el viedo soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viendo -respondio otro monje#ni las banderolas ni el viento lo que s emueve son vuestras mente -dijo el maesto."
