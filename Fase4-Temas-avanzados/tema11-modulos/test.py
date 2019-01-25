e3b2b4ab467c26e2c274a3474565b70cc08319dc
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-25 18:18:41 -0400

    otra forma de importar y ejecutar

--- Fase4-Temas-avanzados/tema11-modulos/test.py
+++ Fase4-Temas-avanzados/tema11-modulos/test.py
@@ -1,5 +1,9 @@
-# importando
-import saludos
+# # importando
+# import saludos
+#
+# # para llamar el saludo de un import
+# saludos.saludar()
 
-# para llamar el saludo de un import
-saludos.saludar()
+# importar un y usar la funciones de otra manera
+from saludos import saludar
+saludar()
