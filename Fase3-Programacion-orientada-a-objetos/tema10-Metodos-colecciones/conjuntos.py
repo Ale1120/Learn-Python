7c70ec59c4b7bd170e5ec933a91adfeb87305a02
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-25 15:21:47 -0400

    discard() para borrar un elemento de un conjunto

--- Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/conjuntos.py
+++ Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/conjuntos.py
@@ -6,3 +6,7 @@ conjunto.add(1)
 conjunto.add(2)
 conjunto.add(3)
 print("agregandos elementos con add: ",conjunto)
+
+# sacar un elemento del conjunto
+conjunto.discard(1)
+print("sacando elemento 1 del conjunto:"conjunto)
