8374bc407b0f962e098c1927e7e51f5715f51c5e
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-25 16:42:03 -0400

    get() para mostrar un valor atraves de su key de un diccionario

---
+++ Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/diccionarios.py
@@ -0,0 +1,9 @@
+#creando diccionario
+colores = {"amarillo":"yellow", "azul":"blue", "verde":"green"}
+colores['amarillo']
+
+# mostrar elemento
+print("mostrar elemento con get()",colores.get('amarillo',"no se encuentra"))
+
+# ver si una keys se  encuentra en el diccionario
+"amarillo" in colores
