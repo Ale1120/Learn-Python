75dd83041e240bae6b233fcedf0d6412e23c6c91
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-25 17:14:34 -0400

    keys(), values() y item() para mostar llave: valor de un diccionario

--- Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/diccionarios.py
+++ Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/diccionarios.py
@@ -7,3 +7,12 @@ print("mostrar elemento con get()",colores.get('amarillo',"no se encuentra"))
 
 # ver si una keys se  encuentra en el diccionario
 "amarillo" in colores
+
+# mostrar keys
+print("mostanado las clave de un diccionario keys():",colores.keys())
+
+# mostrar valores
+print("mostanado las valores de un diccionario values():",colores.values())
+
+# mostrar keys y values
+print("Mostando keys y values en tuplas con item(): ",colores.item())
