b1afffe62cf2338a734d4b01058538b0bdc1f1f9
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-25 17:44:46 -0400

    introduccion a ejerccio 2

--- Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/ejerccios/ejerccios.py
+++ Fase3-Programacion-orientada-a-objetos/tema10-Metodos-colecciones/ejerccios/ejerccios.py
@@ -1,4 +1,4 @@
-# ejerccios 1
+# ejerccios 1 string
 texto = "un dia que el viedo soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viendo -respondio otro monje#ni las banderolas ni el viento lo que se mueve son vuestras mente -dijo el maestro"
 
 lineas = texto.split("#")
@@ -12,3 +12,9 @@ for i,linea in enumerate(lineas):
 
 for linea in lineas:
     print(linea)
+
+print("======================================")
+
+# ejerccios 2 lista
+
+lista = [29,-5,-12,17,5,24,5,12,23,16,12,5,-15,17]
