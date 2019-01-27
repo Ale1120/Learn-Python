315438f01c0266eaf27841f4bcbbca81f9aefb3d
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-26 21:21:32 -0400

    creando saludos con funcion y clase

---
+++ Fase4-Temas-avanzados/tema11-modulos/paquetes/paquete/saludos.py
@@ -0,0 +1,8 @@
+# este es un modulo con funciones que saludan
+def saludar():
+    print("hola, te estoy saludando desde la funcion saludar del modulo saludos")
+
+
+class Saludo():
+    def __init__(self):
+        print("Hola te estoy saludando desde el __init__ de la clase del modulo saludos")
