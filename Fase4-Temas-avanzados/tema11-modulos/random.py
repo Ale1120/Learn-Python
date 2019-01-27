d013afbb27ea44adad505b9026cf5ca59e933328
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-27 18:40:22 -0400

    numero aletorios con radom() y uniform()

--- Fase4-Temas-avanzados/tema11-modulos/random.py
+++ Fase4-Temas-avanzados/tema11-modulos/random.py
@@ -1,1 +1,7 @@
 import random
+
+# numero aleatorio flotante >= 0 y < 1.0
+print("Utilizando random.random() nos regresa numero random >= 0 y < 1.0 ",random.random())
+
+# numero aleatorio flotante entre dos numeros >= n1 y < n2
+print("Utilizando random.uniform() nos regresa numero random >= n1 y < n2 ",random.uniform(1,10))
