819b450dac4880bce17e407fe69f4b61c779df3b
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-26 21:32:50 -0400

    creando funciones despedirse y clase despedida

--- Fase4-Temas-avanzados/tema11-modulos/paquetes/paquete/adios/despedida.py
+++
@@ -1,8 +0,0 @@
-# este es un modulo con funciones que saludan
-def saludar():
-    print("hola, te estoy saludando desde la funcion saludar del modulo saludos")
-
-
-class Saludo():
-    def __init__(self):
-        print("Hola te estoy saludando desde el __init__ de la clase del modulo saludos")
---
+++ Fase4-Temas-avanzados/tema11-modulos/paquetes/paquete/adios/despedidas.py
@@ -0,0 +1,8 @@
+# este es un modulo con funciones que despiden
+def despedirse():
+    print("Adios, me estoy despidiendo desde la funcion despedirse del modulo despedidas")
+
+
+class Despedida():
+    def __init__(self):
+    print("Adios, me estoy despidiendo desde el init de la clase Despedida del modulo despedidas")
