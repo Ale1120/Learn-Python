5c53943a67a505542aeb25348096a805b7456ec8
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-26 23:23:11 -0400

    pip3 install paquete-0.1.tar.gz para installar paquete distribuible

--- Fase4-Temas-avanzados/tema11-modulos/paquetes/setup.py
+++ Fase4-Temas-avanzados/tema11-modulos/paquetes/setup.py
@@ -1,6 +1,8 @@
 # setuptools por defecto
 # paquete distribuible
 # para crear el distribuible python setup.py sdist
+# para usar stuptools installar con pip
+# para instalar paguete distribuible pip3 install paquete-0.1.tar.gz
 from setuptools import setup
 
 setup(
