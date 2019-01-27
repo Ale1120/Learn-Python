7d4d3bb4633fba8e98256016fb8e493f9b4f184b
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-26 23:50:50 -0400

    usando most_common para ver elementos mas comunes

--- Fase4-Temas-avanzados/collections.py
+++ Fase4-Temas-avanzados/collections.py
@@ -6,4 +6,12 @@ lista = [1,2,3,4,1,23,1,2,1]
 print("Mostrando un contador de numeros en la lista con counter",Counter(lista))
 
 palabra = 'palabra'
-print("Mostrando un contador de los caracteres en la lista con counter",Counter(lista))
+print("Mostrando un contador de los caracteres en la lista con counter",Counter(palabra))
+
+# ejemplo de uso de Counter con cadenas
+animales = "gato perros canarios perros canarios perro"
+c = Counter(animales.split())
+print ("Mostrando contador de string con Counter",c)
+
+# elementos mas comunes con most_common(valor)
+c.most_common()
