9044b2a34f9c74dab99d7b33841eb13cc9659f15
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-27 18:28:36 -0400

    usando math.fsum() para sumas mas efectivas

--- Fase4-Temas-avanzados/tema11-modulos/math.py
+++ Fase4-Temas-avanzados/tema11-modulos/math.py
@@ -10,3 +10,8 @@ print("Redondeo a la altta con math.ceil(): ",math.ceil(3.000001))
 print("EL valor absoluto de -10 es :",abs(-10))
 
 # sumatiorio
+ n = [1,2,3]
+ print("la suma de una lista 1,2,3 con math.fsum():",math.fsum(n))
+#ejemplo de uso
+n = [0.999999,2,3]
+print( "La suma de una lista 0.999999,2,3 con fsum():",math.fsum(n))
