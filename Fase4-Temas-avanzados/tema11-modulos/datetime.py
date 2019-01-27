eca777bd95a76803dfe6989688a0342964b7465b
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-27 16:03:49 -0400

    importando datetime

---
+++ Fase4-Temas-avanzados/tema11-modulos/datetime.py
@@ -0,0 +1,8 @@
+# importando datetime
+import datetime
+
+dt = datetime.datetime.now() # ahora
+print("Mostrando tiempo con datetime.now :",dt)
+print("Mostrando año: ",dt.year)
+print("Mostrando mes: ",dt.moth)
+print("Mostrando dia",dt.day)
