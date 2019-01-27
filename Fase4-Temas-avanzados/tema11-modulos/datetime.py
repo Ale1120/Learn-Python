f6f7afa7b57bd774475acc0fb3d4be2724298c11
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-27 16:11:54 -0400

    imprimiendo hora y fecha con format

--- Fase4-Temas-avanzados/tema11-modulos/datetime.py
+++ Fase4-Temas-avanzados/tema11-modulos/datetime.py
@@ -4,5 +4,12 @@ import datetime
 dt = datetime.datetime.now() # ahora
 print("Mostrando tiempo con datetime.now :",dt)
 print("Mostrando año: ",dt.year)
-print("Mostrando mes: ",dt.moth)
+print("Mostrando mes: ",dt.month)
 print("Mostrando dia",dt.day)
+print("Mostrar hora: ",dt.hour)
+print("Mostrar minuto: ",dt.minute)
+print("Mostrar segundo: ",dt.second)
+print("Mostrar microsegundos: ",dt.microsecond)
+print("Mostrar zona horaria: ",dt.tzinfo)
+print("Imprimiendo la hora {}:{}:{}".format(dt.hour, dt.minut, dt.second))
+print("Imprimiendo la fecha {}/{}/{}".format(dt.day, dt.month, dt.year))
