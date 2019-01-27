12b79f08fe3d3766949774726aba6d81a462378d
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-27 16:40:25 -0400

    importando locate

--- Fase4-Temas-avanzados/tema11-modulos/datetime.py
+++ Fase4-Temas-avanzados/tema11-modulos/datetime.py
@@ -14,6 +14,25 @@ print("Mostrar zona horaria: ",dt.tzinfo)
 print("Imprimiendo la hora {}:{}:{}".format(dt.hour, dt.minut, dt.second))
 print("Imprimiendo la fecha {}/{}/{}".format(dt.day, dt.month, dt.year))
 print("================================")
+
+# Creando fecha manual
 print("!!!Creando fecha manual!!!")
 time = datetime.datetime(2000,1,1,0,0)
 print("Fecha manual:",time)
+
+# formateo con iso
+dt = datetime.datetime.now()
+print("Mostrado el fecha con isoformat(): ", dt.isoformat())
+
+# formateo manual a nuestra propia hora
+# %A dia
+# %d fecha (1-31)
+# %B Meses en letras
+# %Y año
+# %I hora en formato 12 horas
+# %M minutos
+print("!!! Formateando manual mente ")
+print("Mi Datetime formateado es :" dt.strftime("%A %d %B %Y %I:%M"))
+
+# importando locale
+import locale
