1bb770852aa754bb5b7a02ca5c67b0276ffa0cb4
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-26 23:50:29 -0400

    Counter para contar elementos de una lista

---
+++ Fase4-Temas-avanzados/collections.py
@@ -0,0 +1,9 @@
+# importando collections
+from collections import Counter
+
+# usando Counter para contar elementos de una lista
+lista = [1,2,3,4,1,23,1,2,1]
+print("Mostrando un contador de numeros en la lista con counter",Counter(lista))
+
+palabra = 'palabra'
+print("Mostrando un contador de los caracteres en la lista con counter",Counter(lista))
