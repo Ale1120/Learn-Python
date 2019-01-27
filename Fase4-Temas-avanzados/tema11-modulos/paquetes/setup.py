36e79c83b9266529c73b01a7a6a186a0c00c10d9
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-26 23:05:51 -0400

    creando paquete distribuible

--- Fase4-Temas-avanzados/tema11-modulos/paquetes/setup.py
+++ Fase4-Temas-avanzados/tema11-modulos/paquetes/setup.py
@@ -1,3 +1,14 @@
 # setuptools por defecto
-#paquete distribuible
+# paquete distribuible
 from setuptools import setup
+
+setup(
+    name="paquete",
+    version="0.1",
+    description="Este es un paquete de ejemplo",
+    author= "Alejandro Ortegano",
+    author_email= "tus@perritos.com",
+    url="http://tusperrico.com",
+    scripts=[],
+    packeges=["paquete","paquete.adios","paquetes.hola"]
+)
