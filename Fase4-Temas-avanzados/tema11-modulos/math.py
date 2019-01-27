6adae22dbaf68ec519d136d94ca173522ea26a33
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-27 18:24:55 -0400

    valor absoluto

--- Fase4-Temas-avanzados/tema11-modulos/math.py
+++ Fase4-Temas-avanzados/tema11-modulos/math.py
@@ -5,3 +5,8 @@ pi = 3.14159
 print("Redondeo de pi con round(): ",round(pi))
 print("Redondeo a la baja con math.floor(): ",math.floor(3.5))
 print("Redondeo a la altta con math.ceil(): ",math.ceil(3.000001))
+
+# valor absoluto
+print("EL valor absoluto de -10 es :",abs(-10))
+
+# sumatiorio
