67612a45ba1708bad9b7f2a4689e6270a8dfde1b
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-27 18:46:10 -0400

    utilinzando randrange() para numeros alterorios dentro de un rango

--- Fase4-Temas-avanzados/tema11-modulos/random.py
+++ Fase4-Temas-avanzados/tema11-modulos/random.py
@@ -1,7 +1,16 @@
 import random
 
 # numero aleatorio flotante >= 0 y < 1.0
-print("Utilizando random.random() nos regresa numero random >= 0 y < 1.0 ",random.random())
+print("Utilizando random.random() nos regresa numero random flotante>= 0 y < 1.0 ",random.random())
 
-# numero aleatorio flotante entre dos numeros >= n1 y < n2
-print("Utilizando random.uniform() nos regresa numero random >= n1 y < n2 ",random.uniform(1,10))
+# numero aleatorio flotante entre dos numeros >= n1 y < n2.0
+print("Utilizando random.uniform() nos regresa numero random flotante>= n1 y < n2.0 ",random.uniform(1,10))
+
+# aleatorio-rango con numeros enteros >= 0 y < 10
+print("Utilizando random.randrange(10) nos regresa numero random entero >= 0 y < 10 ",random.randrange(10))
+
+# aleatorio-rango con numeros enteros >= n1 y < n2
+print("Utilizando random.randrange(0,101) nos regresa numero random entero >= n1 y < n2 ",random.randrange(0,101))
+
+# ejemplo de uso
+print("Utilizando random.randrange(0,101,2) nos regresa numero random entero pares >= n1 y < n2 ",random.randrange(0,101,2))
