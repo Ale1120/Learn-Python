4597bdde37c16a5ee1d7539cb380a9f7018c90a4
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-27 18:24:07 -0400

    REDONDEANDOC con round(), math.floor() y math.ceil()

---
+++ Fase4-Temas-avanzados/tema11-modulos/math.py
@@ -0,0 +1,7 @@
+import math
+
+# redondeo
+pi = 3.14159
+print("Redondeo de pi con round(): ",round(pi))
+print("Redondeo a la baja con math.floor(): ",math.floor(3.5))
+print("Redondeo a la altta con math.ceil(): ",math.ceil(3.000001))
