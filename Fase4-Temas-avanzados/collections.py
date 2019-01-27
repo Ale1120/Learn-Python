17c585e14b40571296b350a52306a046087e25ec
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-27 00:14:16 -0400

    usnado orderedDict

--- Fase4-Temas-avanzados/collections.py
+++ Fase4-Temas-avanzados/collections.py
@@ -4,15 +4,15 @@ from collections import Counter
 # usando Counter para contar elementos de una lista
 lista = [1,2,3,4,1,23,1,2,1]
 print(lista)
-print("Mostrando un contador de numeros en la lista con counter",Counter(lista))
+print("Mostrando un contador de numeros en la lista con counter: ",Counter(lista))
 
 palabra = 'palabra'
-print("Mostrando un contador de los caracteres en la lista con counter",Counter(palabra))
+print("Mostrando un contador de los caracteres en la lista con counter: ",Counter(palabra))
 
 # ejemplo de uso de Counter con cadenas
 animales = "gato perros canarios perros canarios perro"
 c = Counter(animales.split())
-print ("Mostrando contador de string con Counter",c)
+print ("Mostrando contador de string con Counter: ",c)
 
 # elementos mas comunes con most_common(valor)
 print("Los elementos mas comues de {} son : ".format(animales),c.most_common())
@@ -35,4 +35,14 @@ diccionario = {}
 diccionario = defaultdict(float)
 print("Creando diccionario por defectos:", diccionario)
 print("Buscando una key que no existe en el diccionario: ", diccionario['key'])
-print("Mostrando el diccionario con el valor de la key que le pasamos:",diccionario)
+print("Mostrando el diccionario con el valor de la key que le pasamos: ",diccionario)
+print("==================================================================================")
+
+# ordenando como instroduscamos los valores en diccionario
+print("!!Diccionario ordenas con el orden de introduccion!!")
+from collections import OrderedDict
+n = OrderedDict()
+n['uno'] = 'one'
+n['dos'] = 'two'
+n['tres'] = 'three'
+print("Mostrand el diccionario ordenado con OrderedDict(): ",n)
