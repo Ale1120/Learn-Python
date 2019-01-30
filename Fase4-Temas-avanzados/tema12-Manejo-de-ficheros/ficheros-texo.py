5bf1106e35bf2b6120912ad7ececa605d6a721a9
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-30 13:28:19 -0400

    creando fichero de tipo lectura

--- Fase4-Temas-avanzados/tema12-Manejo-de-ficheros/ficheros-texo.py
+++ Fase4-Temas-avanzados/tema12-Manejo-de-ficheros/ficheros-texo.py
@@ -1,10 +1,17 @@
 # para poder abrir ficheros  con io desde un script
 from io import open
 
-texto = "Una linea con texto\nOrtra linea con texto"
+texto = "Una linea con texto\nOtra linea con texto"
 
-#creando ficheros open(nombre, tipo)
-fichero = open('fichero.txt','w')
+# #creando ficheros open(nombre, tipo) de escritura
+# fichero = open('fichero.txt','w')
+#
+# # escribiendo dentro de un fichero
+# fichero.write(texto)
 
-# escribiendo dentro de un fichero
-fichero.write(texto)
+# crear un fichero de modo lectura
+fichero = open('fichero.txt','r')
+
+# pasando contenido al fichero de modo lectura
+texto = fichero.read()
+print(texto)
