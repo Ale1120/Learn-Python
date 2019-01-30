3183a5de8866599ea9b56a0392597c2a1a88b9be
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-30 10:03:43 -0400

    ejerccios 2 reloj importando datatime, time y os

--- Fase4-Temas-avanzados/tema11-modulos/ejerccios/reloj.py
+++ Fase4-Temas-avanzados/tema11-modulos/ejerccios/reloj.py
@@ -1,1 +1,10 @@
 #ejerccios 2
+import datetime
+import time
+import os # ejecutando mandos de la terminal
+
+while True:
+    os.system("clear")
+    dt = datetime.datetime.now()
+    print("{}:{}:{}".format(dt.hour, dt.minute, dt.second))
+    time.sleep(1)
