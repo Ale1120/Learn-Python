cfa2fc9552850363d09e027ff5a1ae3c9fb0a9ca
osvani <osvacolijim@gmail.com>
2019-03-01 19:56:18 -0400

    Recuperando informacion de un fichero binario con pickle

--- Fase4-Temas-avanzados/tema12-Manejo-de-ficheros/ficheros-objetos-Pickle.py
+++ Fase4-Temas-avanzados/tema12-Manejo-de-ficheros/ficheros-objetos-Pickle.py
@@ -7,3 +7,14 @@ lista = [1,2,3,4,5]
 fichero = open('lista.pckl','wb')
 
 pickle.dump(lista, fichero)
+
+# recuperando el fichero binario
+fichero = open('lista.pckl','rb')
+lista = pickle.load(fichero)
+print("Recuperando el Fichero binario:",lista)
+del(lista)
+
+# colocando el puntero en el inicio
+fichero.seek(0)
+lista = pickle.load(fichero)
+print("Recuperando el Fichero binario, despues de borrar la lista:",lista)
