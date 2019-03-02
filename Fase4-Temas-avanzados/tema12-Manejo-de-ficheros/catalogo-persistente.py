3ade01f2d812fdcbd82888c73a11d603538f6d93
Ale1120 <alejandro.ortegano11@gmail.com>
2019-03-01 21:01:32 -0400

    cargando peliculas en el fichero catalogo

--- Fase4-Temas-avanzados/tema12-Manejo-de-ficheros/catalogo-persistente.py
+++ Fase4-Temas-avanzados/tema12-Manejo-de-ficheros/catalogo-persistente.py
@@ -10,17 +10,42 @@ class Pelicula:
         print("Se ha Creado La Pelicula:", self.titulo)
 
         def __str__(self):
-            retunr '{} ({})'.format(self.titulo, self.lanzamiento)
+            return '{} ({})'.format(self.titulo, self.lanzamiento)
 
 
 class Catalogo:
 
-    def __init__(self, peliculas=[]):
-        self.peliculas = peliculas
+    peliculas = []
+    def __init__(self):
+        self.cargar()
 
     def agregar(self, pelicula):
         self.peliculas.append(pelicula)
+        self.guardar()
 
     def mostrar(self):
         for pelicula in self.peliculas:
             print(pelicula)
+
+    def cargar(self):
+        fichero = open('catalogo.pckl','ab+') # binario y lectura
+        fichero.seek(0) # colocar el puntero en el inicio
+        try:
+            sefl.peliculas = pickle.load(fichero)
+        except:
+            print("El Catalogo Esta Vacio")
+        finally:
+            fichero.close()
+            print("Se han cargado {} peliculas".format( len(self.peliculas) ))
+
+    def guardar(self):
+        fichero = open('catalogo.pckl','wb')
+        pickle.dump(self.peliculas,fichero)
+        fichero.close()
+
+    # Destructor
+    def __del__(self):
+        self.guardar() # guardado automatico
+
+
+c = Catalogo()
