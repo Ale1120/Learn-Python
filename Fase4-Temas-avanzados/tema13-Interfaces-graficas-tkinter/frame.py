3ac1d95c44b96556b37380f7cf16267d435243d9
Ale1120 <alejandro.ortegano11@gmail.com>
2019-03-02 19:18:08 -0400

    colocando estilo al frame y al root

---
+++ Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/frame.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Hola Mundo")
+root.resizable(1,1)
+root.iconbitmap('@ironman.xbm') # forma de mostrar un icono
+
+frame = Frame(root, width=480, height=320) # creando marco
+frame.pack()
+# frame.config(width=480, height=320) # tamaño del frame
+frame.config(cursor="pirate")
+frame.config(bg="lightblue")
+frame.config(bd=25)
+frame.config(relief='sunken')
+
+root.config(cursor="arrow")
+root.config(bg="blue")
+root.config(bd=15)
+root.config(relief='ridge')
+
+
+# Abajo del todo
+root.mainloop()
