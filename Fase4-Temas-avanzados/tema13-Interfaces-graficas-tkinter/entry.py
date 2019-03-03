180361b8163204ee6f05b68c9caa4e3549c5e188
Ale1120 <alejandro.ortegano11@gmail.com>
2019-03-03 14:23:43 -0400

    usando entry para crear formularios en tkinter

--- Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/entry.py
+++ Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/entry.py
@@ -4,8 +4,22 @@ from tkinter import *
 root = Tk()
 
 # creando un entry
+
+label = Label(root, text="Usuario")
+label.grid(row=0, column=0, sticky="e", padx=5, pady=5)
+
 entry = Entry(root)
-entry.pack()
+entry.grid(row=0, column=1, padx=5, pady=5)
+entry.config(justify="right", state="disabled") # configuracion del imput
+
+label2 = Label(root, text="Contraseña")
+label2.grid(row=1, column=0, sticky="e", padx=5, pady=5)
+
+entry2 = Entry(root)
+entry2.grid(row=1, column=1, padx=5, pady=5)
+entry2.config(justify="center", show="*") # show para poner contraseñas
+
+
 
 # Finalemente bucle de la aplicacion
 root.mainloop()
