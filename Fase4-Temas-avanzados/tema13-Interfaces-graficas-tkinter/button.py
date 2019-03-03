3dcecfff704606098c74263efed1f4aa829bde9b
Ale1120 <alejandro.ortegano11@gmail.com>
2019-03-03 15:21:55 -0400

    realizando un calculadora con tkinter

--- Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/button.py
+++ Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/button.py
@@ -1,14 +1,42 @@
 from tkinter import *
 
-def hola():
-    print("Hola Mundo")
+def sumar():
+    r.set( float(n1.get()) - float(n2.get()) )
+    borrar()
 
-def crear_label():
-    Label(root, text="Label Creada Dinamicamente").pack()
+def resta():
+    r.set( float(n1.get()) - float(n2.get()) )
+    borrar()
+
+def producto():
+    r.set( float(n1.get()) * float(n2.get()) )
+    borrar()
+
+def  borrar():
+    n1.set("")
+    n2.set("")
+
 
 # configuracion de la raiz
 root = Tk()
+root.config(bd=15)
 
-Button(root, text="Clicame", command=crear_label).pack()
+n1 = StringVar()
+n2 = StringVar()
+r = StringVar()
+
+Label(root, text="Numero 1").pack()
+Entry(root, justify="center", textvariable=n1).pack() # primer numero
+Label(root, text="Numero 2").pack()
+Entry(root, justify="center", textvariable=n2).pack() # segundo numero
+Label(root, text="\nResultado").pack()
+Entry(root, justify="center", textvariable=r, state="disabled").pack() # resultado
+
+Label(root, text="").pack()
+Button(root, text="Sumar", command=sumar).pack(side="left")
+Button(root, text="Resta", command=resta).pack(side="left")
+Button(root, text="Producto", command=producto).pack(side="left")
+
+
 # Finalmente bucle de la aplicacion
 root.mainloop()
