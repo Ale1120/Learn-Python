0d8d485c61f571fe1f982a2bd0eba3d6d0f22051
Ale1120 <alejandro.ortegano11@gmail.com>
2019-03-02 20:05:36 -0400

    utilizando packt y config

--- Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/frame.py
+++ Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/frame.py
@@ -7,13 +7,15 @@ root.resizable(1,1)
 root.iconbitmap('@ironman.xbm') # forma de mostrar un icono
 
 frame = Frame(root, width=480, height=320) # creando marco
-frame.pack()
-# frame.config(width=480, height=320) # tamaño del frame
+frame.pack(fill='both', expand='1')
+#frame.pack(side="bottom",  ancher ='w') # parametros buttom, etc.
+#frame.config(width=480, height=320) # tamaño del frame
 frame.config(cursor="pirate")
 frame.config(bg="lightblue")
 frame.config(bd=25)
 frame.config(relief='sunken')
 
+# en root
 root.config(cursor="arrow")
 root.config(bg="blue")
 root.config(bd=15)
