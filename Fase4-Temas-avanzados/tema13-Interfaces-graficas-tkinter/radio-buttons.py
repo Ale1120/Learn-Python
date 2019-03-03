9d1cb647f045c3225214d869e966dc81eee0993f
Ale1120 <alejandro.ortegano11@gmail.com>
2019-03-03 16:14:50 -0400

    usando RadioButton

--- Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/radio-buttons.py
+++ Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/radio-buttons.py
@@ -3,6 +3,10 @@ from tkinter import *
 # configuracion de la raiz
 root = Tk()
 
+Radiobutton(root, text="Opcion 1").pack()
+Radiobutton(root, text="Opcion 1").pack()
+Radiobutton(root, text="Opcion 1").pack()
+
 
 # Finalmente bucle de la aplicacion
 root.mainloop()
