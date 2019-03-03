3d4ea626483056cc04581afc4ed52c7d17b51afc
Ale1120 <alejandro.ortegano11@gmail.com>
2019-03-03 15:01:06 -0400

    creando un text

--- Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/text.py
+++ Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/text.py
@@ -3,7 +3,8 @@ from tkinter import *
 # configuracion de la raiz
 root = Tk()
 
-
-
+texto = Text(root)
+texto.pack()
+texto.config(width=30, height=10, font=("Consolas",12), padx=5, pady=5) # lee caracteres no tamñano
 # Finalmente bucle de la aplicacion
 root.mainloop()
