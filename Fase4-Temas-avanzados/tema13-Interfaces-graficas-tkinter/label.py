cc0734c582f49476ec20ab89c5a1ad24f58cf3f7
Ale1120 <alejandro.ortegano11@gmail.com>
2019-03-02 20:21:04 -0400

    colocandole efectos a los label

--- Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/label.py
+++ Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/label.py
@@ -3,12 +3,14 @@ from tkinter import *
 # configuracion de la raiz
 root = Tk()
 
-# cofinguracion de un marco
-frame = Frame(root, width=480, height=320)
-frame.pack()
 
-label = Label(frame, text="¡Hola Mundo!")
-label.place(x=100, y=100)
+
+Label(root, text="¡Hola Mundo!").pack(anchor="nw")
+label = Label(root, text="¡Hola alejandro !")
+label.pack(anchor="center")
+Label(root, text="¡Adios!").pack(anchor="se")
+
+label.config(bg="green", fg="blue", font=("verdana", 24))
 
 # Finalmente bucle de la aplicacion
 root.mainloop()
