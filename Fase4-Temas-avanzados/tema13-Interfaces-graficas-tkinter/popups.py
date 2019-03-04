4a8e7452ab30399a979059139b86bb4761b1ae15
Ale1120 <alejandro.ortegano11@gmail.com>
2019-03-04 10:40:40 -0400

    creando venta de askquestion emergente

--- Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/popups.py
+++ Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/popups.py
@@ -3,7 +3,14 @@ from tkinter import messagebox as MessageBox # cambiar el nombre de
 
 def test():
     # creando ventana emergente
-    MessageBox.showerror("Error!", "Ha ocurrido un error inesperado")
+    # MessageBox.showwinfo("Hola!", "Hola Mundo.")
+    # MessageBox.showwarning("Alerta!", "Seccion solo para administradores.")
+    # MessageBox.showerror("Erro!", "Ha ocurrido un error inesperado")
+
+    # ventana de eleccion
+    resultado = MessageBox.askquestion("Salir","¿Estas seguro si desea salir sin guardar")
+    if resultado == "yes":
+        root.destroy()
 
 # configuracion de la raiz
 root = Tk()
