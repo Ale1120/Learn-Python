e345f792525c0477c30c6d22f7bb0c0262b80981
Ale1120 <alejandro.ortegano11@gmail.com>
2019-03-04 10:32:53 -0400

    creando venta de infomacion emergente

--- Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/popups.py
+++ Fase4-Temas-avanzados/tema13-Interfaces-graficas-tkinter/popups.py
@@ -1,7 +1,14 @@
 from tkinter import *
+from tkinter import messagebox as MessageBox # cambiar el nombre de
+
+def test():
+    # creando ventana emergente
+    MessageBox.showinfo("Hola!"," Hola Mundo")
 
 # configuracion de la raiz
 root = Tk()
+
+Button(root, text="Clicame", command=test).pack()
 
 # Finalmente bucle de la aplicacion
 root.mainloop()
